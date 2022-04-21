a24cf62d1a4ea60bc71559b4da60f2761acf18cd
bsipiczki <hu>
2022-04-21 16:53:44 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FinancialTableComponent } from './ui/financial-table/financial-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FinancialTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui/financial-table/financial-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-table',
+  templateUrl: './financial-table.component.html',
+  styleUrls: ['./financial-table.component.scss']
+})
+export class FinancialTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
